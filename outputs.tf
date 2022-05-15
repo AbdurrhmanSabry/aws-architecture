a1de764f8b86f62f649339bd447ee51ef83226e7
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-15 16:33:39 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -28,33 +28,33 @@ output "public_route_table_id" {
 output "private_route_table_id" {
     value = module.network.private_route_table_id
 }
-output "public_key" {
-    value = tls_private_key.private_key_pair
-    sensitive = true
-}
-output "pem_secret_id" {
-    value = aws_secretsmanager_secret.private_key.id
-    sensitive = true
-}
-output "pem_secret_string" {
-    value = tls_private_key.private_key_pair.private_key_pem
-    sensitive = true
-}
+# output "public_key" {
+#     value = tls_private_key.private_key_pair
+#     sensitive = true
+# }
+# output "pem_secret_id" {
+#     value = aws_secretsmanager_secret.private_key.id
+#     sensitive = true
+# }
+# output "pem_secret_string" {
+#     value = tls_private_key.private_key_pair.private_key_pem
+#     sensitive = true
+# }
 
-output "pub_secret_id" {
-    value = aws_secretsmanager_secret.public_key.id
-    sensitive = true
-}
-output "pub_secret_string" {
-    value =  aws_key_pair.public_key_pair.public_key
-    sensitive = true
-}
+# output "pub_secret_id" {
+#     value = aws_secretsmanager_secret.public_key.id
+#     sensitive = true
+# }
+# output "pub_secret_string" {
+#     value =  aws_key_pair.public_key_pair.public_key
+#     sensitive = true
+# }
 output "bastion_sq" {
     value = aws_security_group.Bastion.id
 }
-output "key_name" {
-    value = aws_key_pair.public_key_pair.key_name
-}
+# output "key_name" {
+#     value = aws_key_pair.public_key_pair.key_name
+# }
 output "application_sq" {
     value = aws_security_group.PrivateSec.id
 }
