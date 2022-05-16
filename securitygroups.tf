bca79fed63eb17570ce1a67d44f8ddbacb132512
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-16 19:42:39 +0200

    allowing http

--- securitygroups.tf
+++ securitygroups.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "Bastion" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    description      = "http from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
@@ -32,6 +40,13 @@ resource "aws_security_group" "PrivateSec" {
     description      = "ssh from VPC"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = [var.vpc_cidr]
   }
