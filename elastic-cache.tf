3114f063ee449bec8287fd9530616f078d68b83c
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-17 20:26:49 +0200

    rds and elastic cache

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -3,7 +3,6 @@ resource "aws_elasticache_cluster" "cache_cluster" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
   engine_version       = "6.x"
   port                 = 6379
   security_group_ids = [aws_security_group.cache-sg.id]
