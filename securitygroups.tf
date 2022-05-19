de5c035beb2ecf086bb1895aced0b263d2cea806
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-19 19:46:33 +0200

    sg for rds

--- securitygroups.tf
+++ securitygroups.tf
@@ -104,7 +104,7 @@ resource "aws_security_group" "rds-sg" {
     from_port        = 3306
     to_port          = 3306
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [var.vpc_cidr]
 
   }
 
