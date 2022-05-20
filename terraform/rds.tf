a3083fc105c1dc09508187196d21c1bde06a227d
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-20 22:28:01 +0200

    rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "mydb" {
   allocated_storage    = 10
   engine               = "mysql"
-  engine_version       = "8.0.28"
+  engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  db_name                 = "mydb"
   username             = "admin"
   password             = "admin123456789"
+  parameter_group_name = "default.mysql5.7"
   db_subnet_group_name = aws_db_subnet_group.mydb_sub_group.name
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
