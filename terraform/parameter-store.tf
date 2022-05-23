2a136124cbae1b263125214abf6d0a57e718ff64
Abdurrhman Sabry <abdurrhmansabry@gmail.com>
2022-05-24 00:17:02 +0200

    ssm naming

--- terraform/parameter-store.tf
+++ terraform/parameter-store.tf
@@ -1,12 +1,12 @@
 
 resource "aws_ssm_parameter" "rds" {
-  name  = "rds_endpoint"
+  name  = "${var.name_environment}/rds-database/address"
   type  = "SecureString"
   value = aws_db_instance.mydb.address
 }
 
 resource "aws_ssm_parameter" "elasticahe" {
-  name  = "elasticahe-address"
+  name  = "${var.name_environment}/elasticahe-redis/address"
   type  = "SecureString"
   value =  aws_elasticache_replication_group.single-node.primary_endpoint_address
 }
